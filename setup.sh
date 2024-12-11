b9aba3b308e57a2249b1f8ed7461a1b4dceed2bf
abaisero <andrea.baisero@gmail.com>
2024-12-11 16:13:36 -0500

    workstation updates

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ ln -s "$lnoptions" "$(realpath tmux.sessions)" "$HOME/.tmux.sessions"
 # ln -s "$lnoptions" $(realpath vimrc) $HOME/.vimrc
 
 # nvim
-ln -s "$lnoptions" "$(realpath nvim)" "$HOME/.config/nvim"
+ln -s "$lnoptions" "$(realpath nvim)" -t "$HOME/.config"
 
 # nvim efm-langserver
 mkdir -p "$HOME/.config/efm-langserver"
@@ -30,18 +30,18 @@ ln -s "$lnoptions" "$(realpath black)" "$HOME/.config/black"
 ln -s "$lnoptions" "$(realpath isort.cfg)" "$HOME/.isort.cfg"
 
 # rubocop
-mkdir -p "$HOME/.config/rubocop"
-ln -s "$lnoptions" "$(realpath rubocop.yml)" "$HOME/.config/rubocop/config.yml"
+# mkdir -p "$HOME/.config/rubocop"
+# ln -s "$lnoptions" "$(realpath rubocop.yml)" "$HOME/.config/rubocop/config.yml"
 
 # ruby gem
 # ln -s "$lnoptions" "$(realpath gemrc)" "$HOME/.gemrc"
 
 # firefox custom css
-ln -s "$lnoptions" "$(realpath firefox.userChrome.css)" "$HOME/snap/firefox/common/.mozilla/firefox/tw3f5bux.default/chrome/userChrome.css"
+#ln -s "$lnoptions" "$(realpath firefox.userChrome.css)" "$HOME/snap/firefox/common/.mozilla/firefox/tw3f5bux.default/chrome/userChrome.css"
 
 # alacritty
-ln -s "$lnoptions" "$(realpath alacritty.toml)" "$HOME/.config/alacritty/alacritty.toml"
-ln -s "$lnoptions" "$(realpath alacritty.yml)" "$HOME/.config/alacritty/alacritty.yml"
+#ln -s "$lnoptions" "$(realpath alacritty.toml)" "$HOME/.config/alacritty/alacritty.toml"
+#ln -s "$lnoptions" "$(realpath alacritty.yml)" "$HOME/.config/alacritty/alacritty.yml"
 
 # zathura
 # ln -s "$lnoptions" $(realpath zathurarc) "$HOME/.config/zathura/zathurarc"
